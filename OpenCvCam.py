a8e362ba6762e86a79d4fad4119c6202cf34a723
D P <24988751+JoeTester1965@users.noreply.github.com>
2023-09-20 19:52:22 +0100

    tuned before writing valid motion algo

--- OpenCvCam.py
+++ OpenCvCam.py
@@ -10,7 +10,8 @@ import os
 
 class VideoStreamWidget(object):
     def __init__(self, name, uri, gaussian_kernel_size, pixel_delta_threshold, 
-                    fps, masks_directory, minimum_motion_screen_percent):
+                    fps, masks_directory, minimum_motion_screen_percent,
+                    image_rescaling_factor):
         self.fps = fps
         self.masks_directory = masks_directory
         self.minimum_motion_screen_percent = minimum_motion_screen_percent
@@ -30,53 +31,72 @@ class VideoStreamWidget(object):
         while True:
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
+            
+            if self.status:
+                if image_rescaling_factor != 1.0:
+                    self.frame = cv2.resize(self.frame, (0, 0), fx = image_rescaling_factor, fy = image_rescaling_factor, interpolation = cv2.INTER_LINEAR)
+                if self.last_frame is None:
+                    self.last_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                    self.last_frame = cv2.GaussianBlur(self.last_frame, (gaussian_kernel_size,gaussian_kernel_size), 0)
+                    width,height = self.last_frame.shape
+                    self.image_pixels = width * height
+                    mask_template_name = f'{masks_directory}/{self.name}-mask.jpg'
+                    if os.path.exists(mask_template_name):
+                        #use existing mask
+                        self.mask = cv2.imread(mask_template_name)
+                        self.mask = cv2.cvtColor(self.mask, cv2.COLOR_BGR2GRAY)
+                        width,height = self.mask.shape
+                        if width*height != self.image_pixels:
+                            #video size has changed, cannot use current mask
+                            self.mask = None
+                    else:
+                        #save new candidate template mask                 
+                        candidate_mask_template_name = f'{masks_directory}/{self.name}-mask.candidate.jpg'
+                        cv2.imwrite(candidate_mask_template_name, self.frame)
 
-            if self.last_frame is None:
-                self.last_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-                self.last_frame = cv2.GaussianBlur(self.last_frame, (gaussian_kernel_size,gaussian_kernel_size), 0)
-                width,height = self.last_frame.shape
-                self.image_pixels = width * height
-                mask_template_name = f'{masks_directory}/{self.name}-mask.jpg'
-                if os.path.exists(mask_template_name):
-                    #use existing mask
-                    self.mask = cv2.imread(mask_template_name)
-                    self.mask = cv2.cvtColor(self.mask, cv2.COLOR_BGR2GRAY)
-                else:
-                    #save new candidate template mask                 
-                    candidate_mask_template_name = f'{masks_directory}/{self.name}-mask.candidate.jpg'
-                    cv2.imwrite(candidate_mask_template_name, self.frame)
+                self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
-            self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                if self.mask is not None:
+                    self.gray = cv2.bitwise_and(self.gray,self.mask)
 
-            if self.mask is not None:
-                self.gray = cv2.bitwise_and(self.gray,self.mask)
+                self.gray = cv2.GaussianBlur(self.gray, ( gaussian_kernel_size,gaussian_kernel_size ), 0)
+                frameDelta = cv2.absdiff(self.last_frame, self.gray)
+                self.last_frame = self.gray
+                thresh = cv2.threshold(frameDelta, self.pixel_delta_threshold, 255, cv2.THRESH_BINARY)[1]
+                kernel = np.ones((delta_frame_dilation,delta_frame_dilation),np.uint8)
+                thresh = cv2.dilate(thresh, kernel, iterations = 1)
+                cv2.imshow("Debug", thresh)
+                cv2.waitKey(1)
+                contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+                contours = contours[0] if len(contours) == 2 else contours[1]
+                contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-            self.gray = cv2.GaussianBlur(self.gray, (gaussian_kernel_size,gaussian_kernel_size), 0)
-            frameDelta = cv2.absdiff(self.last_frame, self.gray)
-            self.last_frame = self.gray
-            thresh = cv2.threshold(frameDelta, self.pixel_delta_threshold, 255, cv2.THRESH_BINARY)[1]
-            contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-  
-            contours = contours[0] if len(contours) == 2 else contours[1]
-            contours = sorted(contours, key=cv2.contourArea, reverse=True)
+                if display_contour_debug:
+                    for contour in contours:
+                        x,y,w,h = cv2.boundingRect(contour)
+                        cv2.rectangle(self.frame,(x,y),(x+w,y+h),(255,255,255),3)
 
-            if display_contour_debug:
-                for contour in contours:
+                for contour in contours[:motion_contours_to_consider ]:
                     x,y,w,h = cv2.boundingRect(contour)
-                    cv2.rectangle(self.frame,(x,y),(x+w,y+h),(255,255,255),3)
+                    if (((w * h) / self.image_pixels) * 100) > minimum_motion_screen_percent:
+                        logger.info("%s : significant object at %d,%d:%d,%d", self.name, x,y,w,h)
+                        cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),5)
 
-            for contour in contours[:top_n_objects_to_consider]:
-                x,y,w,h = cv2.boundingRect(contour)
-                if (((w * h) / self.image_pixels) * 100) > minimum_motion_screen_percent:
-                    logger.info("%s object at %d,%d:%d,%d", self.name, x,y,w,h)
-                    cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),5)
-
-             # sleep within framerate for each camera (separate thread)
-            time.sleep(1/fps/2)
+                # sleep within framerate for each camera (separate thread)
+                time.sleep(1/fps/2)
+            else:
+                #
+                # Camera down !
+                #
+                pass
     
     def show_frame(self):
-        cv2.imshow(self.name, self.frame)
-        key = cv2.waitKey(1)
+        try:
+            cv2.imshow(self.name, self.frame)
+            key = cv2.waitKey(1)
+        except:
+            pass
 
 def read_config(config_file):
     global config
@@ -89,12 +109,14 @@ read_config(sys.argv[1])
 cameras = dict(config['cameras']) 
 	
 #motion config
+image_rescaling_factor = float(config['motion']['image_rescaling_factor'])
 gaussian_kernel_size = int(config['motion']['gaussian_kernel_size'])
 pixel_delta_threshold = int(config['motion']['pixel_delta_threshold'])
+delta_frame_dilation = int(config['motion']['delta_frame_dilation']) 
 fps = int(config['motion']['fps'])
 minimum_motion_screen_percent = float(config['motion']['minimum_motion_screen_percent'])
 display_contour_debug = int(config['motion']['display_contour_debug'])
-top_n_objects_to_consider = int(config['motion']['top_n_objects_to_consider'])
+motion_contours_to_consider  = int(config['motion']['motion_contours_to_consider'])
 
 #general config
 masks_directory = config['general']['masks_directory']
@@ -124,7 +146,8 @@ streams = {}
 
 for name,uri in cameras.items():
     streams[name] = VideoStreamWidget(name, uri, gaussian_kernel_size, pixel_delta_threshold, 
-                                            fps, masks_directory, minimum_motion_screen_percent)
+                                            fps, masks_directory, minimum_motion_screen_percent,
+                                                image_rescaling_factor)
 while True:
     for name,uri in cameras.items():
         try:
